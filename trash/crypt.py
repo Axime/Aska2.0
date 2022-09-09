02d61f3ccbcf1c8b351a0f30c5fe6dc75351cc44
Axime <ivashka_2020@list.ru>
2022-09-09 18:01:14 +0500

    crypo

--- trash/crypt.py
+++ trash/crypt.py
@@ -6,24 +6,18 @@
 """
 import cryptocode
 
-class CriptingInformation ():
 
-    def cript (self,get_data):
+class CriptingInformation:
+
+    def cript(self, get_data):
         self.get_data = get_data
-        encrypted_information = cryptocode.encrypt(get_data,"password")
+        encrypted_information = cryptocode.encrypt(get_data, "password")
         return encrypted_information
-
-
-
 
 
 obj = CriptingInformation()
 data_for_cript = "example"
 print(obj.cript(data_for_cript))
-
-
-
-
 
 """str_encoded = cryptocode.encrypt("I am okay","wow")
 print(str_encoded)
