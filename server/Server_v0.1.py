ca872517295af030b69e913b8e4f60c84e9d6fa8
Axime <ivashka_2020@list.ru>
2022-09-10 16:46:20 +0500

    server on flask

--- server/Server_v0.1.py
+++ server/Server_v0.1.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, request
+from peewee import *
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return "home page"
+
+@app.route("/test", methods=["POST"])
+def test():
+    reqest_data = request.get_json()
+    try:
+        lang = reqest_data["language"]
+        frame = reqest_data["framework"]
+
+        py_vers = reqest_data["version_info"]["python"]
+
+        example = reqest_data['examples'][0]
+
+        bool_test = reqest_data["boolean_test"]
+        return """
+                The language value is: {}
+                The framework value is: {}
+                The Python version is: {}
+                The item at index 0 in the example list is: {}
+                The boolean value is: {}""".format(lang, frame, py_vers, example, bool_test)
+    except KeyError as e:
+        return f"Блять этого нет{e}"
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
