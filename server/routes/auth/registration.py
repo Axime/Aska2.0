dc7679211bbb38a0f9484d28e6d93aace7070b3a
Axime <ivashka_2020@list.ru>
2022-09-11 22:24:48 +0500

    add hashing

--- server/routes/auth/registration.py
+++ server/routes/auth/registration.py
@@ -1,3 +1,5 @@
+import jwt
+import random
 from flask import Flask, request
 import json
 import sqlite3
@@ -6,48 +8,28 @@ def registration_user(app: Flask):
     @app.route("/api/auth/registration", methods=["POST"])
     def user_reg():
         request_data = request.get_json()
-        try:
-            first_name = request_data["first_name"]
-            last_name = request_data["last_name"]
-            email = request_data["email"]
-            password = request_data["password"]
-            password_repeat = request_data["password_repeat"]
-            secure_key = request_data["__secure_key"]
+
+        #Парсинг JSON на переменные
+        first_name= request_data["first_name"]
+        last_name = request_data["last_name"]
+        email = request_data["email"]
+        password = request_data["password"]
+        password_repeat = request_data["password_repeat"]
+        secure_key = request_data["__secure_key"]
 
 
-            #Registration body
-            con = sqlite3.connect("users.db")
-            c = con.cursor()
-            try:
-                c.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)",
-                          (first_name, last_name, email, password, password_repeat, secure_key))
-                print(f"Данные добавлены {first_name} {last_name} {email} {password} {password_repeat} {secure_key}")
-                con.commit()
-                c.execute(f"SELECT rowid, * from users WHERE rowid >= 1")
-                items = c.fetchall()
-                print("Список пользователей")
-                for item in items:
-                    id, first_name, last_name, email, password, password_repeat, secure_key = item
-                    print(f'{id} {first_name} {last_name} {email} {password} {password_repeat} {secure_key}')
+        #Заглушки
+        user_token = "test_token"
+        user_id = 0
+        log_hash =  "test_hash"
 
-                con.close()
-            except KeyError as s:
-                print(s)
+        algorithm = "HS256"
+        information_for_encode = {"test" : "test"}
 
-            except Exception as e:
-                print(e)
+        information_for_decript = jwt.encode(information_for_encode, "key", algorithm=algorithm)
+        print(jwt.decode(information_for_decript, "key", algorithms=["HS256"]))
 
-
-
-            return "yes!"
-
-#В случае ошибки выбрасываем это
-        except KeyError as e:
-            return json.dumps({
-                "error": str(e),
-                "error_code": 0
-            }), 400, {
-                       'Content-Type': 'application/json'
-                   }
+        #Ретурн функции user_reg
+        return information_for_decript
 
     return user_reg
