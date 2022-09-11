cee11f904141292f78b3eafcd35304f74804a6d3
Axime <ivashka_2020@list.ru>
2022-09-11 13:58:17 +0500

    add users

--- server/routes/auth/registration.py
+++ server/routes/auth/registration.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import json
-import datausers
+import sqlite3
 
 def registration_user(app: Flask):
     @app.route("/api/auth/registration", methods=["POST"])
@@ -14,8 +14,31 @@ def registration_user(app: Flask):
             password_repeat = request_data["password_repeat"]
             secure_key = request_data["__secure_key"]
 
-            req = first_name,last_name,email,password,password_repeat,secure_key
-            datausers.add_user("".join(req))
+
+            #Registration body
+            con = sqlite3.connect("users.db")
+            c = con.cursor()
+            try:
+                c.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)",
+                          (first_name, last_name, email, password, password_repeat, secure_key))
+                print(f"Данные добавлены {first_name} {last_name} {email} {password} {password_repeat} {secure_key}")
+                con.commit()
+                c.execute(f"SELECT rowid, * from users WHERE rowid >= 1")
+                items = c.fetchall()
+                print("Список пользователей")
+                for item in items:
+                    id, first_name, last_name, email, password, password_repeat, secure_key = item
+                    print(f'{id} {first_name} {last_name} {email} {password} {password_repeat} {secure_key}')
+
+                con.close()
+            except KeyError as s:
+                print(s)
+
+            except Exception as e:
+                print(e)
+
+
+
             return "yes!"
 
 #В случае ошибки выбрасываем это
